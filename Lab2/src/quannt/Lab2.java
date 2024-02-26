36f63cb916195a1826b57a6ad876ae9c94ccf86d
Henrius73 <quanntkt2003@gmail.com>
2024-02-26 21:37:40 +0700

    lab2_finish

--- Lab2/src/quannt/Lab2.java
+++ Lab2/src/quannt/Lab2.java
@@ -33,4 +33,41 @@ public class Lab2 {
 	            System.out.println("Nghiệm của phương trình là x = " + x);
 	        }
 	    }
+	public static void Bai2() {
+		double a,b,c;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập hệ số a: ");
+		a = scanner.nextDouble();
+		System.out.print("Nhập hệ số b: ");
+		b = scanner.nextDouble();
+		System.out.print("Nhập hệ số c: ");
+		c = scanner.nextDouble();
+		if (a == 0) {
+	            
+	            if (b == 0) {
+	                if (c == 0) {
+	                    System.out.println("Phương trình có vô số nghiệm.");
+	                } else {
+	                    System.out.println("Phương trình vô nghiệm.");
+	                }
+	            } else {
+	                double x = -c / b;
+	                System.out.println("Phương trình có nghiệm duy nhất x = " + x);
+	            }
+		} else {
+            double delta = b * b - 4 * a * c;
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm.");
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Phương trình có nghiệm kép x = " + x);
+            } else {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Phương trình có hai nghiệm phân biệt:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
+	}
 }
