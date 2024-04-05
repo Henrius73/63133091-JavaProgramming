4a100b57359dea0a9871d6a69b2ae8c903e3524b
Henrius73 <quanntkt2003@gmail.com>
2024-04-06 00:44:16 +0700

    Lab7

---
+++ Lab7/src/quannt/MainBai3.java
@@ -0,0 +1,11 @@
+package quannt;
+
+public class MainBai3 {
+	public static void main(String[] args) {
+        SinhVienIT svIT = new SinhVienIT("Nguyen Duc Hung", 8.5, 7.0, 9.0);
+        svIT.xuat();
+
+        SinhVienBiz svBiz = new SinhVienBiz("Le Duc Hoang", 7.5, 8.0);
+        svBiz.xuat();
+    }
+}
---
+++ Lab7/src/quannt/VuongBai1.java
@@ -0,0 +1,14 @@
+package quannt;
+
+public class VuongBai1 extends ChuNhatBai1{
+	VuongBai1(double canh){
+		super(canh, canh);
+	}
+	
+	@Override
+	void xuat() {
+        System.out.println("Canh: " + dai);
+        System.out.println("Dien tich: " + getDienTich());
+        System.out.println("Chu vi: " + getChuVi());
+    }
+}
